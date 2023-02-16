6d1ed262e152ff687eb48046cc4ad17ea38fb915
SwatiKashyap12 <kashyapswati1261@gmail.com>
2023-02-16 16:25:28 +0530

    FEb

--- force-app/main/default/lwc/kanchana/kanchana.js
+++ force-app/main/default/lwc/kanchana/kanchana.js
@@ -10,219 +10,231 @@ const QUERY_USER_ENDPOINT_URL='https://api.github.com/search/users?q=';
 
 export default class GitListViewComponent extends LightningElement {
 
-    @api personName;
-    selecteduser ='';
-    selecteduserArray=[];
-    retrivedusers=[];
-    subscription = null;
-    
-    retriveduserName='';
+@api personName;
+selecteduser ='';
+selecteduserArray=[];
+retrivedusers=[];
+subscription = null;
 
-    @wire(getRecord, { recordId: '0015g000017jnFPAAY', fields: 'Account.Name' })
-    wiredRecord({ error, data }) {
-    if(error){
-      console.log(error) ;
-    }else if(data){
-        console.log(data);
-        this.retriveduserName=data.fields.Name.value;
-    }
-    }
-    @wire(MessageContext)
-    messageContext;
+retriveduserName='';
 
-    connectedCallback() {
-        this.subscribeToMessageChannel();
-    }
+@wire(getRecord, { recordId: '0015g000017jnFPAAY', fields: 'Account.Name' })
+wiredRecord({ error, data }) {
+if(error){
+    console.log(error) ;
+}else if(data){
+    console.log(data);
+    this.retriveduserName=data.fields.Name.value;
+}
+}
+@wire(MessageContext)
+messageContext;
 
-    disconnectedCallback() {
-        this.unsubscribeToMessageChannel();
-    }
-
-    // Encapsulate logic for Lightning message service subscribe and unsubsubscribe
-    subscribeToMessageChannel() {
-        if (!this.subscription) {
-            this.subscription = subscribe(
-                this.messageContext,
-                searchMessage,
-                (message) => this.handleMessage(message),
-                { scope: APPLICATION_SCOPE }
-            );
-        }
-    }
-    async handleMessage(message) {
-       console.log('handleMessage:', message);
-       this.personName=message.searchTerm;
-       let queryEndPoint=QUERY_USER_ENDPOINT_URL+this.personName;
-       try{
-        const RESPONSE=await fetch(queryEndPoint);
-        const USER_LIST=await RESPONSE.json();
-        console.log(USER_LIST.items);
-        this.retrivedusers=USER_LIST.items;
-       }catch(error){
-          console.log(error); 
-      }
-    }
-
-unsubscribeToMessageChannel() {
-        unsubscribe(this.subscription);
-        this.subscription = null;
-    }
-
-    handleonuserclicked(event){
-       
-        console.log(event.detail); 
-        
-    this.selecteduser=event.detail;
-    this.selecteduserArray.push(event.detail);
-      //alert(this.selecteduser);
-    }
-    get showuser(){
-        return this.selecteduser.length != 0 ? true: false;
-    }
-
-
-    async handleSaveUserClick(){
-        console.log('save user in SF');
-
-                try{
-        const issuccess=await insertContact1({accNameList:this.selecteduserArray});
-
-        const evt = new ShowToastEvent({
-            title: 'Records Saved',
-            message: 'Records Saved',
-            variant: 'info',
-        });
-        this.dispatchEvent(evt);
-        
-        console.log('created creation'+issuccess);
-        }catch(error){
-        console.log(error);
-        }
-            }
+connectedCallback() {
+    this.subscribeToMessageChannel();
 }
 
- 
-/*export default class Kanchana extends LightningElement { 
-    @api personName; 
-    retrivedusers=[]; 
-    selectedUser='';
-    subscription = null; 
-    @wire(MessageContext) 
-    messageContext; 
- 
-    connectedCallback() { 
-        this.subscribeToMessageChannel(); 
-    } 
-    disconnectedCallback() { 
-        this.unsubscribeToMessageChannel(); 
-    } 
- // Encapsulate logic for Lightning message service subscribe and unsubsubscribe 
-    subscribeToMessageChannel() { 
-        if (!this.subscription) { 
-            this.subscription = subscribe( 
-                this.messageContext, 
-                searchMessage, 
-                (message) => this.handleMessage(message), 
-                { scope: APPLICATION_SCOPE } 
-            ); 
-        } 
-    } 
-    async handleMessage(message) { 
-       console.log('handleMessage:', message); 
-       this.personName=message.searchTerm; 
-       let queryEndPoint=QUERY_USER_ENDPOINT_URL+this.personName; 
-       try{ 
-        const RESPONSE=await fetch(queryEndPoint); 
-        const USER_LIST=await RESPONSE.json(); 
-        console.log(USER_LIST.items); 
-        this.retrivedusers=USER_LIST.items; 
-       }catch(error){ 
-          console.log(error);  
-      } 
-    } 
-   unsubscribeToMessageChannel() { 
-        unsubscribe(this.subscription); 
-        this.subscription = null; 
-    } 
-     
-    handleOnUserClicked(event){
-        console.log(event.detail);
-        this.selectedUser=event.detail;
-       alert(this.selectedUser);
+disconnectedCallback() {
+    this.unsubscribeToMessageChannel();
+}
+
+// Encapsulate logic for Lightning message service subscribe and unsubsubscribe
+subscribeToMessageChannel() {
+    if (!this.subscription) {
+        this.subscription = subscribe(
+            this.messageContext,
+            searchMessage,
+            (message) => this.handleMessage(message),
+            { scope: APPLICATION_SCOPE }
+        );
     }
+}
+async handleMessage(message) {
+    console.log('handleMessage:', message);
+    this.personName=message.searchTerm;
+    let queryEndPoint=QUERY_USER_ENDPOINT_URL+this.personName;
+    try{
+    const RESPONSE=await fetch(queryEndPoint);
+    const USER_LIST=await RESPONSE.json();
+    console.log(USER_LIST.items);
+    this.retrivedusers=USER_LIST.items;
+    if(this.retrivedusers.length === 0) {
+        throw new Error('No user found');
+    }
+} catch(error) {
+    console.log(error);
+    const toastEvent = new ShowToastEvent({
+        title: 'Please enter any value',
+        message: error.message,
+        variant: 'error',
+    });
+    this.dispatchEvent(toastEvent);
+    }
+}
+
+unsubscribeToMessageChannel() {
+    unsubscribe(this.subscription);
+    this.subscription = null;
+}
+
+handleonuserclicked(event){
+    
+    console.log(event.detail); 
+    
+this.selecteduser=event.detail;
+this.selecteduserArray.push(event.detail);
+    //alert(this.selecteduser);
+}
+get showuser(){
+    return this.selecteduser.length != 0 ? true: false;
+}
+
+
+async handleSaveUserClick(){
+    console.log('save user in SF');
+
+            try{
+    const issuccess=await insertContact1({accNameList:this.selecteduserArray});
+
+    const evt = new ShowToastEvent({
+        title: 'Records Saved',
+        message: 'Record Saved',
+        variant: 'info',
+    });
+    this.dispatchEvent(evt);
+    
+    console.log('created creation'+issuccess);
+    }catch(error){
+    console.log(error);
+    }
+    }
+        
+
+    
+}
+
+
+/*export default class Kanchana extends LightningElement { 
+@api personName; 
+retrivedusers=[]; 
+selectedUser='';
+subscription = null; 
+@wire(MessageContext) 
+messageContext; 
+
+connectedCallback() { 
+    this.subscribeToMessageChannel(); 
+} 
+disconnectedCallback() { 
+    this.unsubscribeToMessageChannel(); 
+} 
+// Encapsulate logic for Lightning message service subscribe and unsubsubscribe 
+subscribeToMessageChannel() { 
+    if (!this.subscription) { 
+        this.subscription = subscribe( 
+            this.messageContext, 
+            searchMessage, 
+            (message) => this.handleMessage(message), 
+            { scope: APPLICATION_SCOPE } 
+        ); 
+    } 
+} 
+async handleMessage(message) { 
+    console.log('handleMessage:', message); 
+    this.personName=message.searchTerm; 
+    let queryEndPoint=QUERY_USER_ENDPOINT_URL+this.personName; 
+    try{ 
+    const RESPONSE=await fetch(queryEndPoint); 
+    const USER_LIST=await RESPONSE.json(); 
+    console.log(USER_LIST.items); 
+    this.retrivedusers=USER_LIST.items; 
+    }catch(error){ 
+        console.log(error);  
+    } 
+} 
+unsubscribeToMessageChannel() { 
+    unsubscribe(this.subscription); 
+    this.subscription = null; 
+} 
+    
+handleOnUserClicked(event){
+    console.log(event.detail);
+    this.selectedUser=event.detail;
+    alert(this.selectedUser);
+}
 }*/
 
 /*import { LightningElement,wire } from 'lwc';
 import {subscribe,MessageContext} from 'lightning/messageService';
 import MyChannel from '@salesforce/messageChannel/MyChannel__c';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
- 
+
 export default class Kanchana extends LightningElement {
-    publisherMessage = '';
-    subscription = null;
- 
-    @wire(MessageContext)
-    messageContext;
- 
-    connectedCallback() {
-        this.handleSubscribe();
+publisherMessage = '';
+subscription = null;
+
+@wire(MessageContext)
+messageContext;
+
+connectedCallback() {
+    this.handleSubscribe();
+}
+
+handleSubscribe() {
+    if (this.subscription) {
+        return;
     }
- 
-    handleSubscribe() {
-        if (this.subscription) {
-            return;
-        }
-        this.subscription = subscribe(this.messageContext, MyChannel, (message) => {
-            console.log(message.message);
-            this.publisherMessage = message.message;
-            this.ShowToast('Success', message.message, 'success', 'dismissable');
-        });
-    }
- 
-    ShowToast(title, message, variant, mode){
-        const evt = new ShowToastEvent({
-            title: title,
-            message:message,
-            variant: variant,
-            mode: mode
-        });
-        this.dispatchEvent(evt);
-    }
-     
+    this.subscription = subscribe(this.messageContext, MyChannel, (message) => {
+        console.log(message.message);
+        this.publisherMessage = message.message;
+        this.ShowToast('Success', message.message, 'success', 'dismissable');
+    });
+}
+
+ShowToast(title, message, variant, mode){
+    const evt = new ShowToastEvent({
+        title: title,
+        message:message,
+        variant: variant,
+        mode: mode
+    });
+    this.dispatchEvent(evt);
+}
+    
 }*/
 
 
 /*export default class Kanchana extends LightningElement {
-    textValue;
-    subscription = null;
-    
-    @wire(MessageContext)
-    messageContext;
+textValue;
+subscription = null;
 
-    connectedCallback() {
-        this.subscribeToMessageChannel();
-    }
+@wire(MessageContext)
+messageContext;
 
-    disconnectedCallback() {
-        this.unsubscribeToMessageChannel();
-    }
+connectedCallback() {
+    this.subscribeToMessageChannel();
+}
 
-    subscribeToMessageChannel() {
-        if (!this.subscription) {
-            this.subscription = subscribe(
-                this.messageContext,
-                searchMessage,
-                (message) => this.handleMessage(message),
-                
-            );
-        }
-    }
-    handleMessage(message) {
-        console.log('handleMessage',message);
-    }
+disconnectedCallback() {
+    this.unsubscribeToMessageChannel();
+}
 
-    unsubscribeToMessageChannel() {
-        unsubscribe(this.subscription);
-        this.subscription = null;
+subscribeToMessageChannel() {
+    if (!this.subscription) {
+        this.subscription = subscribe(
+            this.messageContext,
+            searchMessage,
+            (message) => this.handleMessage(message),
+            
+        );
     }
+}
+handleMessage(message) {
+    console.log('handleMessage',message);
+}
+
+unsubscribeToMessageChannel() {
+    unsubscribe(this.subscription);
+    this.subscription = null;
+}
 }*/
