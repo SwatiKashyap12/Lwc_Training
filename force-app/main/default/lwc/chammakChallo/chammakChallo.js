ebf4ae1a8395e0d0b57467ca2ec0bb71cd314afc
SwatiKashyap12 <kashyapswati1261@gmail.com>
2023-02-16 15:44:15 +0530

    Feb

--- force-app/main/default/lwc/chammakChallo/chammakChallo.js
+++ force-app/main/default/lwc/chammakChallo/chammakChallo.js
@@ -1,6 +1,7 @@
 import { LightningElement,wire} from 'lwc'; 
 import { publish, MessageContext } from 'lightning/messageService'; 
 import searchMessage from '@salesforce/messageChannel/MyChannel__c'; 
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ChammakChallo extends LightningElement { 
 @wire(MessageContext) 
@@ -11,6 +12,7 @@ handleClick(event){
 let inputvalue=this.template.querySelectorAll("lightning-input")[0].value; 
 const payload = { isTermvalid:true, searchTerm:inputvalue }; 
    publish(this.messageContext, searchMessage, payload); 
+   
 } 
 
     
